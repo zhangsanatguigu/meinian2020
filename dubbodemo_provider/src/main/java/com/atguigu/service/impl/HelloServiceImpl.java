26b63376dfb053197417b520f6791e0f0200c107
jlsp-zy <jlsp-zy@163.com>
2020-11-10 16:59:01 +0800

    dubbo框架Helloworld

--- dubbodemo_provider/src/main/java/com/atguigu/service/impl/HelloServiceImpl.java
+++ dubbodemo_provider/src/main/java/com/atguigu/service/impl/HelloServiceImpl.java
@@ -2,12 +2,18 @@ package com.atguigu.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.atguigu.service.HelloService;
+import org.springframework.transaction.annotation.Transactional;
 
 //@Service(loadbalance = "random")
-@Service
+@Service(interfaceClass = HelloService.class)
+//@Service
+@Transactional
 public class HelloServiceImpl implements HelloService {
     @Override
+    // @Transactional
     public String sayHello(String name) {
+        System.out.println("this = " + this);
+        System.out.println("this = " + this.getClass());
         return "8083 Hello,"+name;
     }
 }
