b523954668095211c42ef8253934a438b633a630
jlsp-zy <jlsp-zy@163.com>
2020-11-11 08:05:15 +0800

    dubbo框架Helloworld

--- dubbodemo_provider/src/main/java/com/atguigu/service/impl/HelloServiceImpl.java
+++ dubbodemo_provider/src/main/java/com/atguigu/service/impl/HelloServiceImpl.java
@@ -4,10 +4,11 @@ import com.alibaba.dubbo.config.annotation.Service;
 import com.atguigu.service.HelloService;
 import org.springframework.transaction.annotation.Transactional;
 
+//在使用注解式dubbo开发的过程中，忽然发现Service上只要有@transactional注解或者是配置的事务切面时，该Service不能被dubbo发布。
 //@Service(loadbalance = "random")
 @Service(interfaceClass = HelloService.class)
 //@Service
-@Transactional
+//@Transactional //默认，Spring容器生成JDK动态代理，增加事务切面。proxy-target-class="true"指定通过Cglib生成代理对象。
 public class HelloServiceImpl implements HelloService {
     @Override
     // @Transactional
