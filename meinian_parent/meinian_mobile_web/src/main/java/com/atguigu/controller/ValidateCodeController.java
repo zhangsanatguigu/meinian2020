82756a62bff16925e347f592e48cc3b8a3bcafa8
jlsp-zy <jlsp-zy@163.com>
2020-12-19 15:27:47 +0800

    美年旅游-移动端开发模块-立即预约-山东鼎信发送手机验证码

---
+++ meinian_parent/meinian_mobile_web/src/main/java/com/atguigu/controller/ValidateCodeController.java
@@ -0,0 +1,36 @@
+package com.atguigu.controller;
+
+import com.atguigu.constant.MessageConstant;
+import com.atguigu.constant.RedisMessageConstant;
+import com.atguigu.entity.Result;
+import com.atguigu.utils.SMSUtils;
+import com.atguigu.utils.ValidateCodeUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.JedisPool;
+
+@RestController
+@RequestMapping("/validateCode")
+public class ValidateCodeController {
+
+    @Autowired
+    JedisPool jedisPool;
+
+    @RequestMapping("/send4Order")
+    public Result send4Order(String telephone){
+        Integer code = null;
+        try {
+            code = ValidateCodeUtils.generateValidateCode(6);
+            SMSUtils.sendShortMessage(telephone,code.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, MessageConstant.SEND_VALIDATECODE_FAIL);
+        }
+        System.out.println("发送的手机【+telephone+】验证码:"+ code.toString());
+
+        jedisPool.getResource().setex(telephone+ RedisMessageConstant.SENDTYPE_ORDER,5*60, code.toString());
+
+        return new Result(true,MessageConstant.SEND_VALIDATECODE_SUCCESS);
+    }
+}
---
+++ meinian_parent/meinian_service/src/main/java/com/atguigu/service/impl/OrderServiceImpl.java
@@ -0,0 +1,6 @@
+package com.atguigu.service.impl;
+
+import com.atguigu.service.OrderService;
+
+public class OrderServiceImpl implements OrderService {
+}
