ca4c365b4220e9f0d68d214c975a20763594d880
jlsp-zy <jlsp-zy@163.com>
2020-12-21 20:22:52 +0800

    美年旅游-统计-运营数据统计-导出Excel

--- meinian_parent/meinian_common/src/main/java/com/atguigu/constant/MessageConstant.java
+++ meinian_parent/meinian_common/src/main/java/com/atguigu/constant/MessageConstant.java
@@ -63,6 +63,7 @@ public class MessageConstant {
     public static final String GET_SETMEAL_COUNT_REPORT_FAIL = "获取套餐统计数据失败";
     public static final String GET_BUSINESS_REPORT_SUCCESS = "获取运营统计数据成功";
     public static final String GET_BUSINESS_REPORT_FAIL = "获取运营统计数据失败";
+    public static final String EXPORT_BUSINESS_REPORT_FAIL = "导出运营统计数据失败";
     public static final String GET_SETMEAL_LIST_SUCCESS = "查询套餐列表数据成功";
     public static final String GET_SETMEAL_LIST_FAIL = "查询套餐列表数据失败";
 }
--- meinian_parent/meinian_web/src/main/java/com/atguigu/controller/ReportController.java
+++ meinian_parent/meinian_web/src/main/java/com/atguigu/controller/ReportController.java
@@ -6,9 +6,21 @@ import com.atguigu.entity.Result;
 import com.atguigu.service.MemberService;
 import com.atguigu.service.ReportService;
 import com.atguigu.service.SetmealService;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
 import java.nio.channels.NonWritableChannelException;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -78,4 +90,111 @@ public class ReportController {
             return new Result(false,MessageConstant.GET_BUSINESS_REPORT_FAIL);
         }
     }
+
+    /**
+     * 将运营数据统计导出到Excel
+     * @return
+     */
+    @RequestMapping("/exportBusinessReport")
+    public Result exportBusinessReport(HttpServletRequest request, HttpServletResponse response){
+        try {
+            //获取运营数据统计
+            Map<String, Object> result = reportService.getBusinessReportData();
+            String reportDate = (String)result.get("reportDate");
+            Integer todayNewMember = (Integer)result.get("todayNewMember");
+            Integer totalMember = (Integer)result.get("totalMember");
+            Integer thisWeekNewMember = (Integer)result.get("thisWeekNewMember");
+            Integer thisMonthNewMember = (Integer)result.get("thisMonthNewMember");
+            Integer todayOrderNumber = (Integer)result.get("todayOrderNumber");
+            Integer todayVisitsNumber = (Integer)result.get("todayVisitsNumber");
+            Integer thisWeekOrderNumber = (Integer)result.get("thisWeekOrderNumber");
+            Integer thisWeekVisitsNumber = (Integer)result.get("thisWeekVisitsNumber");
+            Integer thisMonthOrderNumber = (Integer)result.get("thisMonthOrderNumber");
+            Integer thisMonthVisitsNumber = (Integer)result.get("thisMonthVisitsNumber");
+            List<Map> hotSetmeal = (List<Map>)result.get("hotSetmeal");
+
+            //获取模板文件
+            String templateRealPath = request.getSession().getServletContext().getRealPath("/template")+ File.separator+"report_template.xlsx";
+
+            //创建工作簿对象
+            XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream(new File(templateRealPath)));
+
+            //获取页签
+            XSSFSheet sheet = workbook.getSheetAt(0);
+
+            Row row = sheet.getRow(2);
+            row.getCell(5).setCellValue(reportDate);
+
+            row = sheet.getRow(4);
+            row.getCell(5).setCellValue(todayNewMember);//新增会员数（本日）
+            row.getCell(7).setCellValue(totalMember);//总会员数
+
+            row = sheet.getRow(5);
+            row.getCell(5).setCellValue(thisWeekNewMember);//本周新增会员数
+            row.getCell(7).setCellValue(thisMonthNewMember);//本月新增会员数
+
+            row = sheet.getRow(7);
+            row.getCell(5).setCellValue(todayOrderNumber);//今日预约数
+            row.getCell(7).setCellValue(todayVisitsNumber);//今日出游数
+
+            row = sheet.getRow(8);
+            row.getCell(5).setCellValue(thisWeekOrderNumber);//本周预约数
+            row.getCell(7).setCellValue(thisWeekVisitsNumber);//本周出游数
+
+            row = sheet.getRow(9);
+            row.getCell(5).setCellValue(thisMonthOrderNumber);//本月预约数
+            row.getCell(7).setCellValue(thisMonthVisitsNumber);//本月出游数
+
+            int rowNum = 12 ;
+
+            for (Map map : hotSetmeal) {
+                String name = (String)map.get("name");
+                Long setmeal_count = (Long)map.get("setmeal_count");
+                BigDecimal proportion = (BigDecimal)map.get("proportion");
+                row = sheet.getRow(rowNum++);
+                row.getCell(4).setCellValue(name);
+                row.getCell(5).setCellValue(setmeal_count);
+                row.getCell(6).setCellValue(proportion.doubleValue());
+            }
+
+            //通过输出流进行文件下载
+            ServletOutputStream out = response.getOutputStream();
+            //下载的数据类型:excel类型
+            response.setContentType("application/vnd.ms-excel");
+            // 下载形式：附件形式
+            response.setHeader("content-Disposition","attachment;filename=report.xlsx");
+            workbook.write(out);
+            out.flush();
+            out.close();
+            workbook.close();
+
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false,MessageConstant.EXPORT_BUSINESS_REPORT_FAIL);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
